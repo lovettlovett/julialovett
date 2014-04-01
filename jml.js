bb32a8b1d5697f1f870f02511c14cec73c46a798
Julia Lovett <julia.m.lovett@gmail.com>
2014-04-01 14:39:01 -0400

    Initial commit

--- jml.js
+++ jml.js
@@ -1,11 +1,11 @@
-$('.flip').click(function(){
+$('.flip').on("mouseover", function(){
     $(this).find('.card').addClass('flipped').mouseleave(function(){
         $(this).removeClass('flipped');
     });
     return false;
 });
 
-$('.miniflip').click(function(){
+$('.miniflip').on("mouseover", function(){
     $(this).find('.minicard').addClass('miniflipped').mouseleave(function(){
         $(this).removeClass('miniflipped');
     });
